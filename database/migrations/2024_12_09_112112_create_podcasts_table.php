a8cc94f665c5dfa5f699b08032289e134960b2be
dany kivuyo <danykivuyo@gmail.com>
2024-12-09 16:12:12 +0300

    image_url

--- database/migrations/2024_12_09_112112_create_podcasts_table.php
+++ database/migrations/2024_12_09_112112_create_podcasts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('title');
             $table->text('description');
             $table->string('audio_url');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
